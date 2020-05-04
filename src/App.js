b0a5df8b2a25cf552c29c625fce599c775cbac15
SeanMombo <Sean@mombourquette.com>
2020-05-04 14:51:00 -0400

    tons of tweaks on landing page.

--- src/App.js
+++ src/App.js
@@ -10,18 +10,21 @@ import Project from './components/project/project.component';
 import Gridtile from './components/gridtile/gridtile.component'
 import Intro from './components/intro/intro.component';
 
-import MainBanner from './components/main-banner/main-banner.component';
+import MainHeader2 from './components/main-banner2/main-banner2.component';
 import Navbar from './components/navbar/navbar.component'
 import AboutPage from './components/about-page/about-page.component'
 
 class App extends React.Component {
 
+  componentDidMount() {
+    
+  }
 
   render() {
     return (
       <div className="App">
-        
-        <MainBanner/>
+        <Navbar/>
+        <MainHeader2/>
         {/* <AboutPage/> */}
 
       </div>
