e5dc083668a84e3c69dc29ec524f33f27a6af88d
SeanMombo <Sean@mombourquette.com>
2020-05-08 16:27:13 -0400

    Started work on portfolio page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
+import { BrowserRouter as Router,
+  Switch,
+  Route,
+  Link} from 'react-router-dom'
 
-import MiddleBox from './components/middle-box/middle-box.component'
-import Card from './components/card/card.component'
-import Iphone from './components/iphone/iphone.component';
-import Project from './components/project/project.component';
-import Gridtile from './components/gridtile/gridtile.component'
-import Intro from './components/intro/intro.component';
-
-import MainHeader from './components/main-banner/main-banner.component';
+import MainPage from './components/main-page/main-page.component';
+import PortfolioPage from './components/portfolio-page/portfolio-page.component'
 // import MainHeader2 from './components/main-banner2/main-banner2.component';
 import Navbar from './components/navbar/navbar.component'
 import AboutPage from './components/about-page/about-page.component'
@@ -24,11 +22,25 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Router>
+          <Navbar/>
 
+        
+          <Switch>
+            <Route path="/about">
+              <div></div>
+            </Route>
+            
+            <Route path="/portfolio">
+              <PortfolioPage/>
+            </Route>
 
-        <Navbar/>
-        <MainHeader/>
-        {/* <AboutPage/> */}
+            <Route path="/">
+              <MainPage/>
+            </Route>
+          </Switch>
+
+        </Router>
 
       </div>
     );
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-
+import {
+    Link
+  } from "react-router-dom";
 import "./navbar.styles.scss"
 
 const Navbar = () => (
     <div className='navbar'>
-        <a className="navButton name" href="/#">sean mombo</a>
-        <a className="navButton" href="/#">About</a>
+        
+        <Link className="name" to="/">sean mombo</Link>
+        <Link to="/">About</Link>
+        <Link to="/">portfolio</Link>
+        <Link to="/">games</Link>
+        <Link to="/">contact</Link>
+        <Link to="/">resume</Link>
+        {/* <a className="navButton name" href="/#">sean mombo</a> 
+         <a className="navButton" href="/#">About</a>
         <a className="navButton" href="/#">portfolio</a>
         <a className="navButton" href="/#">games</a>
         <a className="navButton" href="/#">contact</a>
-        <a className="navButton" href="/#">resume</a>
+        <a className="navButton" href="/#">resume</a> */}
     </div>
 )
 
