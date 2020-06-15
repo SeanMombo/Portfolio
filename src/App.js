bdd5a3bcd066f30bcc85a805b98abac8cc148bac
SeanMombo <Sean@mombourquette.com>
2020-06-15 19:52:36 -0400

    image-rendering

--- src/App.js
+++ src/App.js
@@ -12,10 +12,37 @@ import PortfolioPage from './components/portfolio-page/portfolio-page.component'
 import Navbar from './components/navbar/navbar.component'
 // import ProjectModal from './components/project-modal/project-modal.component'
 
-class App extends React.Component {
+function watchForHover() {
+  // lastTouchTime is used for ignoring emulated mousemove events
+  // that are fired after touchstart events. Since they're
+  // indistinguishable from real events, we use the fact that they're
+  // fired a few milliseconds after touchstart to filter them.
+  let lastTouchTime = 0
 
+  function enableHover() {
+    if (new Date() - lastTouchTime < 500) return
+    document.body.classList.add('hasHover')
+  }
+
+  function disableHover() {
+    document.body.classList.remove('hasHover')
+  }
+
+  function updateLastTouchTime() {
+    lastTouchTime = new Date()
+  }
+
+  document.addEventListener('touchstart', updateLastTouchTime, true)
+  document.addEventListener('touchstart', disableHover, true)
+  document.addEventListener('mousemove', enableHover, true)
+
+  enableHover()
+}
+
+class App extends React.Component {
+  
   componentDidMount() {
-    
+    watchForHover()
   }
 
   render() {
